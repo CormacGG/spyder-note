000ae32a1351f2863209a016f53f5db5c45076fc
Cormac Gleeson <cormac.gleeson@appfolio.com>
2021-09-26 20:09:27 -0700

    Add some Kivy Files

--- kivytest.py
+++ kivytest.py
@@ -2,14 +2,14 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 
 
-class PongGame(Widget):
+class NoteEditor(Widget):
     pass
 
 
-class PongApp(App):
+class NoteApp(App):
     def build(self):
-        return PongGame()
+        return NoteEditor()
 
 
 if __name__ == '__main__':
-    PongApp().run()
+    NoteApp().run()
